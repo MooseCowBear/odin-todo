2318b81053d827562e2d920f3ff19b7c31b67bf1
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-07-17 11:21:00 -0400

    add project component

---
+++ src/projectComponent.js
@@ -0,0 +1,29 @@
+import { orderByDate } from "./order.js";
+import { createTaskGroup, createTaskItem } from "./subcomponents.js";
+
+export function projectComponent(project, parent) {
+  parent.textContent = "";
+
+  const component = document.createElement('div');
+  const title = document.createElement('h1');
+  title.textContent = "Project";
+  component.appendChild(title);
+
+  const projectDiv = document.createElement('div');
+
+  const tasks = project.getTasks();
+
+  for (const cat in tasks) {
+    //create task group 
+    const group = createTaskGroup(cat);
+
+    //now list the tasks for each category
+    const sortedTasks = orderByDate(tasks[cat]);
+
+    for (const t of sortedTasks) {
+      const item = createTaskItem(t, true);
+      group.appendChild(item);
+    }
+    projectDiv.appendChild(group);
+  }
+}
