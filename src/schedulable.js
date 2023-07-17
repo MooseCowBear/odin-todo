7417a3873db5c316730e5f20a255d41f1491eeab
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-07-17 06:42:14 -0400

    abstract date and time functions into schedulable

---
+++ src/schedulable.js
@@ -0,0 +1,43 @@
+const Schedulable = (date, time) => {
+  const getDate = () => {
+    if (date === "") return null;
+    const timeString = time === "" ? "T23:59:59" : "T" + time + ":00";
+    return new Date(date + timeString);
+  };
+
+  const dateFormatted = () => {
+    if (date === "") return date;
+    const month = date[5] === "0" ? date.slice(6, 7) : date.slice(5, 7);
+    const day = date[8] === "0" ? date.slice(9) : date.slice(8);
+    const year = date.slice(0, 4);
+
+    return month + "/" + day + "/" + year;
+  };
+
+  const timeFormatted = () => {
+    if (time === "") return time;
+    let hour = parseInt(time.slice(0, 2));
+    const min = time.slice(2);
+    const m = hour > 12 ? "pm" : "am";
+    hour = hour > 12 ? hour - 12 : hour;
+    return `${hour}${min} ${m}`;
+  };
+
+  //also need set date, time for editing
+
+  const setDate = (newDate) => {
+    date = newDate;
+  };
+
+  const setTime = (newTime) => {
+    time = newTime;
+  };
+
+  return {
+    getDate,
+    dateFormatted,
+    timeFormatted,
+    setDate,
+    setTime
+  };
+}
